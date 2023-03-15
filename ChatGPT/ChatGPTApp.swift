410fd3af39789093ec71a812a597841405aa4c7e
mxnuchim <manuchimoliver779@gmail.com>
2023-03-15 10:30:22 +0100

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Manuchim Oliver on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
